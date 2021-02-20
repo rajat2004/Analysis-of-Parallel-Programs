817eced5d0ff0620be0be221f1e8c3bb02704b63
Rajat Singhal <rajatsinghal564@gmail.com>
2021-02-20 22:57:00 +0530

    Add Deadlock with Cyclic Barrier example

---
+++ Assgn1/src/DeadlockCyclicBarrier.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+class BarrierThread extends Thread {
+    private CyclicBarrier B1, B2;
+
+    public BarrierThread(CyclicBarrier b1, CyclicBarrier b2) {
+        B1 = b1;
+        B2 = b2;
+    }
+
+    public void run() {
+        System.out.println("Starting " + this.getName());
+        try {
+            B1.await();
+            System.out.println("Thread " + this.getName() + ": Barrier 1 completed");
+            B2.await();
+            System.out.println("Finishing thread " + this.getName());
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class DeadlockCyclicBarrier {
+    public static void main(String[] args) {
+        CyclicBarrier b1 = new CyclicBarrier(2);
+        CyclicBarrier b2 = new CyclicBarrier(2);
+
+        BarrierThread t1 = new BarrierThread(b1, b2);
+        BarrierThread t2 = new BarrierThread(b2, b1);
+
+        t1.start();
+        t2.start();
+    }s on B1, 
+}
